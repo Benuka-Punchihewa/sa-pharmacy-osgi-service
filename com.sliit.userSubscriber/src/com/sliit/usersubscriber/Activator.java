0136bf3a57d015a9b74057f0c78784755673863c
Sumedha Lakmal Bandaranayaka <74480291+sumedha-95@users.noreply.github.com>
2023-03-18 20:52:33 +0530

    added user pub sub

--- com.sliit.userSubscriber/src/com/sliit/usersubscriber/Activator.java
+++ com.sliit.userSubscriber/src/com/sliit/usersubscriber/Activator.java
@@ -16,21 +16,26 @@ public class Activator implements BundleActivator {
 
 	public void start(BundleContext context) throws Exception {
 		System.out.println(
-				"========================Pharmacy Management System Subscriber Service==========================");
+				"========================Pharmacy Management System Subscriber Service==========================\n");
 		serviceReference = context.getServiceReference(UserServicePublish.class.getName());
 		UserServicePublish servicePublish = (UserServicePublish) context.getService(serviceReference);
 
 		try {
-			System.out.println("Enter a option >>>>> \n Option 1 = Create User \n Option 2 = Get User Using Id \n Option 3 = Get User List");
+			System.out.println("Enter an option  \n Option 1 = Create User \n Option 2 = Get User Using Id \n Option 3 = Get User List \n Option 4 = Exit System");
+			
 			int option;
 			Scanner input = new Scanner(System.in);
 
 			// Loop endlessly.
 			while (true) {
+				System.out.println(
+						"===============================================================================================\n");
 				// Ask the user to enter a word.
 				System.out.print("Enter Option: ");
+				
 				option = input.nextInt();
-
+				System.out.println(
+						"===============================================================================================\n");
 				switch (option) {
 				case 1:
 					System.out.print("Enter User Id: ");
@@ -41,7 +46,7 @@ public class Activator implements BundleActivator {
 					String age = input.next();
 					System.out.print("Enter User Contect Number: ");
 					String contect_number = input.next();
-					System.out.println("Enter User Adderess: ");
+					System.out.print("Enter User Adderess: ");
 					String address = input.next();
 
 					User user = servicePublish.creteUser(user_id, username, age, contect_number, address);
@@ -57,6 +62,12 @@ public class Activator implements BundleActivator {
 					String UserId = input.next();
 
 					User user2 = servicePublish.getUsereById(UserId);
+					
+					if (user2 == null) {
+						System.out.println("User not found!");
+						break;
+					}
+					
 					System.out.println("Found User!");
 					System.out.println("User ID:" + user2.getUser_id() + "\tUser Name:" + user2.getUsername()
 							+ "\tUser Age:" + user2.getAge() + "\tUser Contect Number:" + user2.getContect_number()
@@ -72,6 +83,12 @@ public class Activator implements BundleActivator {
 						System.out.println(user1.toString());
 					}
 					break;
+				case 4:
+					System.out.println("Exiting the system...");
+					System.out.println("================================Have a nice day..==================================");
+					System.exit(0);
+					break;
+					
 				default:
 					System.out.println("Please Select a Valid Option!");
 				}
